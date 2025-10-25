61f3b64cdd2c016ee79d4875fbbd33425f04dd10
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-10-26 00:08:33 +0330

    Revamp Fjolsenbanden landing with playful gradients

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import FjolsenbandenPortalShort from "@/components/FjolsenbandenPortalShort";
+
+export default function HomePage() {
+  return <FjolsenbandenPortalShort />;
+}
