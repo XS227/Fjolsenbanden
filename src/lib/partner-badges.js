8108fde9384273be60482699b3db037e5a6fc67d
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-11-21 17:11:16 +0330

    Update partner logos

--- src/lib/partner-badges.js
+++ src/lib/partner-badges.js
@@ -1,12 +1,12 @@
 export const INLINE_PARTNER_BADGES = Object.freeze({
-    lenovo: "https://fjolsenbanden.setaei.com/assets/p-lenovo-zG8s0yqd.png",
-    samsung: "https://fjolsenbanden.setaei.com/assets/p-samsung-CTyK3nvK.png",
-    philips: "https://fjolsenbanden.setaei.com/assets/p-philips-B33Lb_3a.png",
-    komplett: "https://fjolsenbanden.setaei.com/assets/p-komplett-Bpx7vYhC.png",
-    kristiania: "https://fjolsenbanden.setaei.com/assets/p-kristiania-DmhQbukm.png",
-    saily: "https://fjolsenbanden.setaei.com/assets/p-saily-D-MbpSdv.png",
+    lenovo: "https://setaei.com/Fjolsen/partnere/p-lenovo.png",
+    samsung: "https://setaei.com/Fjolsen/partnere/p-samsung.png",
+    philips: "https://setaei.com/Fjolsen/partnere/p-philips.png",
+    komplett: "https://setaei.com/Fjolsen/partnere/p-komplett.png",
+    kristiania: "https://setaei.com/Fjolsen/partnere/p-kristiania.png",
+    saily: "https://setaei.com/Fjolsen/partnere/p-saily.png",
     nki: "https://fjolsenbanden.setaei.com/img/partners/nki-logo-light.svg",
-    trondheimPlay: "https://fjolsenbanden.setaei.com/assets/p-trondheimplay-BQDD3bw9.png",
+    trondheimPlay: "https://setaei.com/Fjolsen/partnere/p-trondheimplay.png",
 });
 
 export function getPartnerBadge(slug) {
