9e6c51f741486046cdb1c3db3641ad330ce58c0c
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-12-15 15:46:16 +0330

    Update fjolsen_secret.php

--- P/fjolsen_secret.php
+++ P/fjolsen_secret.php
@@ -1,8 +1,8 @@
 <?php
 return [
   "VIPPS_ENV" => "prod", // IKKE test
-  "VIPPS_CLIENT_ID" => "",
-  "VIPPS_CLIENT_SECRET" => "",
-  "VIPPS_SUBSCRIPTION_KEY" => "",
-  "VIPPS_MSN" => "",
+  "VIPPS_CLIENT_ID" => "221283b7-650d-493c-a0ac-7065631be784",
+  "VIPPS_CLIENT_SECRET" => "eRD8Q~u0E~zrvL8rDd2c_nG42t24pxtbm3Bg8c5f",
+  "VIPPS_SUBSCRIPTION_KEY" => "b8d72c7cc137c4e97cbea731d89f3c46",
+  "VIPPS_MSN" => "1042384",
 ];
