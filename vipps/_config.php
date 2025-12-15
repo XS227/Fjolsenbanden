553bdb0d0324f1c8c49550718d56e8b992189c61
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-12-15 03:55:59 +0330

    Update _config.php

--- vipps/_config.php
+++ vipps/_config.php
@@ -1,13 +1,8 @@
 <?php
 return [
-  // Produksjon:
-  'base_url' => 'https://api.vipps.no',
-
-  'client_id' => '221283b7-650d-493c-a0ac-7065631be784',
-  'client_secret' => 'eRD8Q~u0E~zrvL8rDd2c_nG42t24pxtbm3Bg8c5f',
-  'subscription_key' => 'b8d72c7cc137c4e97cbea731d89f3c46',
-  'msn' => '1042384',
-
-  'merchant_redirect_url' => 'https://www.fjolsenbanden.no/vipps/return.html',
-  'callback_url' => 'https://www.fjolsenbanden.no/vipps/callback.php',
+  "VIPPS_ENV" => "prod",
+  "VIPPS_CLIENT_ID" => "221283b7-650d-493c-a0ac-7065631be784",
+  "VIPPS_CLIENT_SECRET" => "eRD8Q~u0E~zrvL8rDd2c_nG42t24pxtbm3Bg8c5f",
+  "VIPPS_SUBSCRIPTION_KEY" => "b8d72c7cc137c4e97cbea731d89f3c46",
+  "VIPPS_MSN" => "1042384"
 ];
