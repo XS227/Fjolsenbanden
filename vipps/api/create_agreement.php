39e3badff8775eaf146d1e12a7e301ccc0a02b63
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-12-16 00:07:15 +0330

    Update create_agreement.php

--- vipps/api/create_agreement.php
+++ vipps/api/create_agreement.php
@@ -24,7 +24,7 @@ $payload = [
   "callbackUrl" => "https://fjolsenbanden.no/vipps/callback.php"
 ];
 
-$url = VIPPS_RECURRING_BASE . "/agreements";
+$url = VIPPS_RECURRING_BASE;
 $result = vipps_post_json($url, $payload);
 
 echo json_encode([
