3bbad742615aed55715ad64f206ddebc9fe1004f
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-12-15 23:34:07 +0330

    Update test_secrets.php

--- vipps/api/test_secrets.php
+++ vipps/api/test_secrets.php
@@ -1,4 +1,7 @@
 <?php
-$s = require "/P/fjolsen_secret.php";
-echo "OK";
+require_once __DIR__ . "/_bootstrap.php";
 
+echo json_encode([
+    "client_id" => defined("VIPPS_CLIENT_ID") && VIPPS_CLIENT_ID !== "",
+    "client_secret" => defined("VIPPS_CLIENT_SECRET") && VIPPS_CLIENT_SECRET !== "",
+]);
